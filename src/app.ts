6d0c6e9f3e7fa4aadff12ebac4eadb5b13e314be
canji53 <canji.preengineer@gmail.com>
2020-11-19 11:55:12 +0900

    :sparkles:  mongooseを追加

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,46 @@
+/* eslint-disable no-console */
 import dotenv from 'dotenv'
 import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
-
-// routing //
+import mongoose from 'mongoose'
+// custom routing
 import router from './routes'
 
 dotenv.config()
 
+/* mongodb */
+// connection_url
+const isValue = (value: string | undefined): string => value || ''
+const user: string = isValue(process.env.MONGO_USERNAME)
+const password: string = isValue(process.env.MONGO_PASSWORD)
+const dbName: string = isValue(process.env.MONGO_DBNAME)
+const dbUrl = `mongodb+srv://${user}:${password}@cluster0.ndrhy.mongodb.net/${dbName}?retryWrites=true&w=majority`
+// options
+const dbOptions = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+}
+// connect
+mongoose
+  .connect(dbUrl, dbOptions)
+  .then(() => {
+    console.log('successfully connected to the database')
+  })
+  .catch((err) => {
+    console.log('error connecting to the database')
+    console.log(err)
+    process.exit()
+  })
+
+/* express */
 const app = express()
+const port = process.env.PORT || 3000
 app.use(helmet())
 app.use(cors())
-
-// use express standard modules, not body-parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-const port = process.env.PORT || 3000
-
+app.use(express.json()) // use express standard modules
+app.use(express.urlencoded({ extended: true })) // use express standard modules
 app.use('/', router)
-
 app.listen(port)
 console.log(`Listen port ${port}`)
