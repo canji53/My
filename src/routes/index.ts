74924fb24e541c3b588112a246079977a0a6fd33
canji53 <canji.preengineer@gmail.com>
2020-11-20 11:56:58 +0900

    :sprkles:  skillのget,post,deleteのメソッドを新規追加

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
-import IMySkill from '../interfaces/IMySkill'
-import MySkillService from '../services/MySkillService'
+import ISkill, { ISkillResponse } from '../interfaces/ISkill'
+import SkillService from '../services/SkillService'
 
 const app = express()
 app.use(helmet())
@@ -10,29 +10,51 @@ app.use(cors())
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req: express.Request, res: express.Response) => {
   res.status(200).send({ message: 'Hello, world' })
 })
 
-router.get('/skill', (req, res, next) => {
-  const service = new MySkillService()
-  service
-    .reads()
-    .then((result) => res.status(200).send(result))
-    .catch(next)
-})
+router.get(
+  '/skill',
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const service = new SkillService()
+    service
+      .reads()
+      .then((result) => res.status(200).send(result))
+      .catch(next)
+  }
+)
 
-router.post('/skill', (req, res, next) => {
-  const mySkill = req.body as IMySkill // Deprecated but unavoidable ...
-  const service = new MySkillService()
-  service
-    .create(mySkill)
-    .then((result) => res.status(200).send(result))
-    .catch(next)
-})
+router.post(
+  '/skill',
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const skill = req.body as ISkill // Deprecated but unavoidable ...
+    const service = new SkillService()
+    service
+      .create(skill)
+      .then((result: ISkillResponse) =>
+        res.status(result.status).send({ message: result.message })
+      )
+      .catch(next)
+  }
+)
+
+router.delete(
+  '/skill/:name',
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const { name } = req.params
+    const service = new SkillService()
+    service
+      .delete(name)
+      .then((result: ISkillResponse) =>
+        res.status(result.status).send({ message: result.message })
+      )
+      .catch(next)
+  }
+)
 
 // 404 not found response
-app.use((req, res) => {
+app.use((req: express.Request, res: express.Response) => {
   res.status(404)
   res.render('error', {
     param: {
