58dcdafa9bf48f14b63035673bf5dbc0b140d2af
canji53 <canji.preengineer@gmail.com>
2020-11-20 11:53:39 +0900

    :recycle:  app処理をクラス化

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,39 @@
-/* eslint-disable no-console */
 import dotenv from 'dotenv'
 import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
-import mongoose from 'mongoose'
-// custom routing
 import router from './routes'
 
 dotenv.config()
 
-/* mongodb */
-// connection_url
-const isValue = (value: string | undefined): string => value || ''
-const user: string = isValue(process.env.MONGO_USERNAME)
-const password: string = isValue(process.env.MONGO_PASSWORD)
-const dbName: string = isValue(process.env.MONGO_DBNAME)
-const dbUrl = `mongodb+srv://${user}:${password}@cluster0.ndrhy.mongodb.net/${dbName}?retryWrites=true&w=majority`
-// options
-const dbOptions = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-}
-// connect
-mongoose
-  .connect(dbUrl, dbOptions)
-  .then(() => {
-    console.log('successfully connected to the database')
-  })
-  .catch((err) => {
-    console.log('error connecting to the database')
-    console.log(err)
-    process.exit()
-  })
+class App {
+  public app: express.Application
 
-/* express */
-const app = express()
-const port = process.env.PORT || 3000
-app.use(helmet())
-app.use(cors())
-app.use(express.json()) // use express standard modules
-app.use(express.urlencoded({ extended: true })) // use express standard modules
-app.use('/', router)
-const server = app.listen(port)
-server.timeout = 1000 * 30 * 1 // 1000[ms](1[s]) * 30 * 1 = 30[s]
-console.log(`Listen port ${port}`)
+  private timeoutSeconds: number
+
+  private port: number
+
+  constructor() {
+    this.timeoutSeconds = 30
+    this.port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
+    this.app = express()
+    this.app.use(helmet())
+    this.app.use(cors())
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: true }))
+    this.routes() // routing
+  }
+
+  private routes = (): void => {
+    this.app.use('/', router)
+  }
+
+  public start = (): void => {
+    const server = this.app.listen(this.port)
+    server.timeout = 1000 * this.timeoutSeconds // 1000[ms](1[s]) * 30 = 30[s]
+    // eslint-disable-next-line no-console
+    console.log(`Listen port ${this.port}`)
+  }
+}
+
+export default App
